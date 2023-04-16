9e2da4433ecfb9b618b80875029f6470f0e2e7a3
Roland <roland.pinke9@gmail.com>
2023-04-16 11:24:49 +0200

    Updated requirements.txt

--- DjangoHoneypot/settings.py
+++ DjangoHoneypot/settings.py
@@ -16,6 +16,9 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
