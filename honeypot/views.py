f3e47b290c5d22c46e8ef708143390595946d01f
Roland <roland.pinke9@gmail.com>
2023-04-16 21:22:51 +0200

    Updated requirements.txt

--- honeypot/views.py
+++ honeypot/views.py
@@ -9,24 +9,31 @@ import time
 
 from honeypot.models import LoginAttemptExtended
 
+class RemoteAddrMiddleware(object):
 
-@receiver(honeypot, sender=LoginAttempt)
-def my_handler(sender, **kwargs):
-    for key, value in kwargs.items():
-        if key == "request":
-            ip_address, is_routable = get_client_ip(value)
-            response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
-            time.sleep(1)
-            instance = LoginAttemptExtended(
-                username=value.POST.get('username'),
-                password=value.POST.get('password'),
-                country=response.get("country_name"),
-                city=response.get("city"),
-                session_key=value.session.session_key,
-                ip_address=ip_address,
-                user_agent=value.META.get('HTTP_USER_AGENT'),
-                path=value.get_full_path(),
-            )
-            instance.save()
-            #print(response.get("country_name"), response.get("city"))
+    @receiver(honeypot, sender=LoginAttempt)
+    def my_handler(sender, **kwargs):
+        for key, value in kwargs.items():
+            if key == "request":
+                print("TUTUTU", value.META)
+                if 'HTTP_X_FORWARDED_FOR' in value.META:
+                    ip_address = value.META['HTTP_X_FORWARDED_FOR'].split(',')[0].strip()
+                    value.META['REMOTE_ADDR'] = ip_address
+                elif 'REMOTE_ADDR' in value.META:
+                    ip_address = value.META['REMOTE_ADDR']
+                else:
+                    ip_address, is_routable = get_client_ip(value)
+                response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
+                time.sleep(1)
+                instance = LoginAttemptExtended(
+                    username=value.POST.get('username'),
+                    password=value.POST.get('password'),
+                    country=response.get("country_name"),
+                    city=response.get("city"),
+                    session_key=value.session.session_key,
+                    ip_address=ip_address,
+                    user_agent=value.META.get('HTTP_USER_AGENT'),
+                    path=value.get_full_path(),
+                )
+                instance.save()
 
